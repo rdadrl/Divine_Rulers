e14769c4c85d21095c9f5d1972018754b472efa5
EmmaVdw0511 <emmavandewouwer@gmail.com>
2019-05-22 19:56:17 +0200

    3D Launcher of the Open Loop

---
+++ titan/src/main/java/solarsysf/LandingPhase3DLauncherOpenLoop.java
@@ -0,0 +1,26 @@
+package solarsysf;
+
+import javafx.application.Application;
+import javafx.stage.Stage;
+import solarsystem.RocketLanderClosedLoop;
+import solarsystem.RocketLanderOpenLoopVerlet;
+import solarsystem.SolarSystem;
+import utils.Date;
+import utils.Vector3D;
+
+public class LandingPhase3DLauncherOpenLoop extends Application {
+    @Override
+    public void start(Stage primaryStage) throws Exception {
+        try {
+            SolarSystem solarSystem = new SolarSystem(); //Solar System Instance
+            Date date = new Date(2000, 0, 1, 0, 0, 0);
+
+            Application landingPhaseApp = new LandingPhase3d(new RocketLanderOpenLoopVerlet( new Vector3D(1000, 200000
+                            , 0),
+                    new Vector3D(0, 0, 0), date), date);
+            landingPhaseApp.start(new Stage()); //start with a empty stage (or you may go ahead and give some specs)
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
+}
