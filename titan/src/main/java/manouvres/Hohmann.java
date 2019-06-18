aa3d43c04322e58ed2224e18bfa9d5a37092b559
= <timdi@icloud.com>
2019-06-18 17:25:38 +0200

    Blabla

--- titan/src/main/java/manouvres/Hohmann.java
+++ titan/src/main/java/manouvres/Hohmann.java
@@ -1,8 +1,14 @@
 package manouvres;
 
+import solarsystem.rocket.Trajectory;
+
 /**
  *
  *
  */
 public class Hohmann {
+
+    public static Trajectory computeImpulse() {
+        return null;
+    }
 }
